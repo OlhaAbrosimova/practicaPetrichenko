42a2513f5ec44ce20fe7e3f582b2324cc6ee29eb
Olha <abrosimova2020@ukr.net>
2022-09-13 15:48:07 +0300

    practica 1 part lesson19

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+//1//
 
+const numberOfFilms = +prompt("How many films have you watched?", "");
+console.log(numberOfFilms);
+
+//2//
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false,
+};
+
+//3//
+
+const filmName1 = prompt("Name one of the last movie you watched?", ""),
+	filmGrade1 = prompt("What is your grade for this film?", ""),
+	filmName2 = prompt("Name one of the last movie you watched?", ""),
+	filmGrade2 = prompt("What is your grade for this film?", "");
+
+personalMovieDB.movies[filmName1] = filmGrade1;
+personalMovieDB.movies[filmName2] = filmGrade2;
+
+console.log(personalMovieDB);
