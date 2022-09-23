7f29f159cc640eb963b1bce10b03cf0cb0288cf6
Olha <abrosimova2020@ukr.net>
2022-09-23 16:25:41 +0300

    practica23.09

--- index.js
+++ index.js
@@ -434,3 +434,352 @@ console.log(counter);
 console.log(Object.keys(options));
 
 console.log(Object.keys(options).length);
+
+let a = 5,
+	b = a;
+
+b = b + a;
+
+console.log(b);
+console.log(a);
+
+// const obj = {
+// 	a: 5,
+// 	b: 1
+// };
+
+// const copy = obj;
+
+// copy.a = 10;
+
+// console.log(copy);
+// console.log(obj);
+
+function copy(mainObj) {
+	let objCopy = {};
+
+	let key;
+	for (key in mainObj) {
+		objCopy[key] = mainObj[key];
+	}
+	return objCopy;
+}
+
+const numbers = {
+	a: 2,
+	b: 5,
+	c: {
+		x: 7,
+		y: 4,
+	},
+};
+
+const newNumbers = copy(numbers);
+
+newNumbers.b = 10;
+newNumbers.c.x = 10;
+// console.log(newNumbers);
+// console.log(numbers);
+
+const add = {
+	d: 17,
+	e: 20,
+};
+
+// let commonObj = Object.assign(numbers, add);
+// // console.log(commonObj);
+// numbers.c.y = 22;
+
+// console.log(numbers);
+// console.log(commonObj);
+// console.log(add);
+
+const clone = Object.assign({}, add);
+
+clone.d = 20;
+
+console.log(add);
+console.log(clone);
+
+const oldArray = ["a", "b", "c"];
+const newArray = oldArray.slice(0, 2);
+
+newArray[1] = "xfgdfgdfg";
+console.log(newArray);
+console.log(oldArray);
+
+const video = ["youtube", "vimeo", "rutube"],
+	blogs = ["wordpress", "livejournal", "blogger"],
+	internet = [...video, ...blogs, "vk", "facebook"];
+
+console.log(internet);
+
+function log(a, b, c) {
+	console.log(a);
+	console.log(b);
+	console.log(c);
+}
+
+const num = [2, 5, 7];
+
+log(...num);
+
+const array = ["a", "b"];
+
+const newAarray = [...array];
+newAarray[0] = "c";
+
+console.log(newAarray);
+console.log(array);
+
+const q = {
+	one: 1,
+	two: 2,
+};
+
+const newObj = { ...q };
+console.log(newObj);
+
+//задача 1 после 35.
+const personalPlanPeter = {
+	name: "Peter",
+	age: "29",
+	skills: {
+		languages: ["ru", "eng"],
+		programmingLangs: {
+			js: "20%",
+			php: "10%",
+		},
+		exp: "1 month",
+	},
+};
+
+function showExperience(plan) {
+	const { exp } = plan.skills;
+	return exp;
+}
+
+console.log(showExperience(personalPlanPeter));
+
+//задача 2
+
+// function showProgrammingLangs(plan) {
+
+// 	let str = "";
+// 	const { programmingLangs } = plan.skills;
+
+// 	for (let key in programmingLangs) {
+// 	str += `Язык ${key} изучен на ${programmingLangs[key]}\n`;
+// 	}
+// 	return str;
+// }
+
+// console.log(showProgrammingLangs(personalPlanPeter));
+
+//задача3
+// const personalPlanPeter = {
+// 	name: "Peter",
+// 	age: "29",
+// 	skills: {
+// 		languages: ["ru", "eng"],
+// 		programmingLangs: {
+// 			js: "20%",
+// 			php: "10%",
+// 		},
+// 		exp: "1 month",
+// 	},
+// 	showAgeAndLangs: function (plan) {
+// 		const { age } = plan;
+// 		const { languages } = plan.skills;
+// 		let str = `Мне ${age} и я владею языками: `;
+
+// 		languages.forEach(function (lang) {
+// 			str += `${lang.toUpperCase()} `;
+// 		});
+// 		return str;
+// 	},
+// };
+
+// console.log(personalPlanPeter.showAgeAndLangs(personalPlanPeter));
+
+//1) Напишите функцию showFamily, которая будет принимать в себя массив строк и возвращать сообщение в нужном формате.
+
+//howFamily(family)  => 'Семья состоит из: Peter Ann Alex Linda'
+
+//Имена подставляются автоматически из массива. Если массив пустой, то выводится сообщение 'Семья пуста'
+
+// const family = ["Peter", "Ann", "Alex", "Linda"];
+
+// function showFamily(arr) {
+// 	let str = "";
+
+// 	arr.length === 0 ? (str = `Семья пуста`) : (str = `Семья состоит из: `);
+
+// 	arr.forEach((member) => {
+// 		str += `${member} `;
+// 	});
+
+// 	return str;
+// }
+
+// console.log(showFamily(family));
+
+//2 задача
+
+// const favoriteCities = ["liSBon", "ROME", "miLan", "Dublin"];
+
+// function standardizeStrings(arr) {
+// 	let str = "";
+
+// 	arr.forEach(function (city) {
+// 		str += `${city}\n`;
+// 	});
+
+// 	console.log(str.toLowerCase());
+// }
+
+// standardizeStrings(favoriteCities);
+
+// //2 задача 2-й вариант решения
+
+// const favoriteCities = ["liSBon", "ROME", "miLan", "Dublin"];
+
+// function standardizeStrings(arr) {
+// 	let str = "";
+// 	for (let i = 0; i < arr.length; i++) {
+// 		str += `${arr[i].toLowerCase()}\n`;
+// 	}
+// 	return str;
+// }
+// console.log(standardizeStrings(favoriteCities));
+
+// const favoriteCities = ["liSBon", "ROME", "miLan", "Dublin"];
+
+// function standardizeStrings(arr) {
+// 	arr.forEach((city) => {
+// 		console.log(city.toLowerCase());
+// 	});
+// }
+
+// standardizeStrings(favoriteCities);
+
+//next задача.
+
+// const someString = "This is some strange string";
+// //reverse(someString) => 'gnirts egnarts emos si sihT'
+
+// function reverse(str) {
+// 	if (typeof str == 'string') {
+// 	let arrayOfString = str.split('');
+// 	const reversed = arrayOfString.reverse();
+// 	let newString = reversed.join('');
+// 	return newString;}
+// 	else {return 'ошибка';}
+// }
+
+// console.log(reverse(someString));
+
+// const someString = "This is some strange string";
+// //reverse(someString) => 'gnirts egnarts emos si sihT'
+
+// function reverse(str) {
+// 	if (typeof str !== "string") {
+// 		return "ошибка";
+// 	}
+
+// 	console.log(str.length);
+
+// 	let newStr = "";
+// 	for (let i = str.length - 1; i >= 0; i--) {
+// 		newStr += str[i];
+// 	}
+// 	return newStr;
+// }
+
+// console.log(reverse(someString));
+
+//след задача
+// const baseCurrencies = ["USD", "EUR"];
+// const additionalCurrencies = ["UAH", "RUB", "CNY"];
+
+// function availableCurr(arr, missingCurr) {
+// 	let str = "";
+// 	arr.length === 0
+// 		? (str = `Нет доступных валют`)
+// 		: (str = `Доступные валюты:\n`);
+
+// 	arr.forEach(function (curr, i) {
+// 		if (curr !== missingCurr) {
+// 			str += `${curr}\n`;
+// 		}
+// 	});
+// 	return str;
+// }
+
+// console.log(availableCurr([...baseCurrencies, ...additionalCurrencies], "CNY"));
+// console.log(availableCurr([], "CNY"));
+
+///////////////
+// const someString = "This is some strange string";
+
+// let newString = "";
+// function reverse(str) {
+// 	if (typeof str !== "string") {
+// 		return `Ошибка!`;
+// 	}
+// 	for (let i = str.length - 1; i >= 0; i--) {
+// 		newString += str[i];
+// 	}
+// 	return newString;
+// }
+
+// console.log(reverse(someString));
+
+// ///////////////////////////
+// const baseCurrencies = ["USD", "EUR"];
+// const additionalCurrencies = ["UAH", "RUB", "CNY"];
+// let str = '';
+
+// function availableCurr(arr, missingCurr) {
+// 	if (arr.length === 0 ) {
+// 		str = `Нет доступных валют`;
+// 	} else {str = `Доступные валюты:\n`;}
+
+// 	// arr.forEach(function(curr) {
+// 	// 	if (curr !== missingCurr) {
+
+// 	// 	}
+// 	// 	str += `${curr}\n`;
+// 	// });
+
+// 	for (let i = 0; i < arr.length; i++) {
+// 		if (arr[i] === missingCurr) {
+// 		continue;
+// 		}
+// 		str += `${arr[i]}\n`;
+// 	}
+// return str;
+// }
+
+// console.log(availableCurr([...baseCurrencies, ...additionalCurrencies], 'CNY'));
+
+const baseCurrencies = ["USD", "EUR"];
+const additionalCurrencies = ["UAH", "RUB", "CNY"];
+let str = "";
+
+function availableCurr(arr, missingCurr) {
+	if (arr.length === 0) {
+		str = `Нет доступных валют`;
+	} else {
+		str = `Доступные валюты:\n`;
+	}
+
+	arr.forEach(function (curr) {
+		if (curr !== missingCurr) {
+			str += `${curr}\n`;
+		}
+	});
+	return str;
+}
+
+console.log(availableCurr([...baseCurrencies, ...additionalCurrencies], "CNY"));
